a4924fb20b312fb94869264d42501d61ea064a7a
infordigi <infordigidevelopment@gmail.com>
2018-03-24 19:28:47 -0300

    #12 ratificacao do Schema.sql

--- dbSchema.sql
+++ dbSchema.sql
@@ -5,27 +5,27 @@
 CREATE DATABASE IF NOT EXIST db_prestador;
 
 ---------------------------------------------------------------------------
--- TABELA tb_clientes
--- DROP TABLE tb_clientes
+-- TABELA tb_usuarios
+-- DROP TABLE tb_usuarios
 ---------------------------------------------------------------------------
 CREATE TABLE IF NOT EXISTS tb_clientes(
-	cli_code BIGSERIAL PRIMARY KEY,
-	cli_name VARCHAR(50) NOT NULL,
-	cli_email VARCHAR(50) UNIQUE NOT NULL,
-	cli_user VARCHAR(20) NOT NULL,
-	cli_pass VARCHAR(24) NOT NULL,
-	cli_status INTEGER NOT NULL
+	usu_code BIGSERIAL PRIMARY KEY,
+	usu_name VARCHAR(50) NOT NULL,
+	usu_email VARCHAR(50) UNIQUE NOT NULL,
+	usu_user VARCHAR(20) NOT NULL,
+	usu_pass VARCHAR(24) NOT NULL,
+	usu_status INTEGER NOT NULL
 );
 
 --------------------------------------------------------------------------
--- INSERT tb_clientes
--- DROP TABLE tb_clientes
+-- INSERT tb_usuarios
+-- DROP TABLE tb_usuarios
 --------------------------------------------------------------------------
 INSERT INTO 
 tb_clientes (
-	cli_name,
-	cli_email,
-	cli_user,
-	cli_pass,
-	cli_status
+	usu_name,
+	usu_email,
+	usu_user,
+	usu_pass,
+	usu_status
 ) VALUES ('Renato Nascimento', 'infordigirn@gmail.com', 'infordigi', '123456', 1);
